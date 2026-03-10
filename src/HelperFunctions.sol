1ae016f7e47f7fcad71d4cf93f84165edf5fda4a
LordDregg <oluwatosinodejide121@gmail.com>
2026-03-10 07:26:29 +0100

    major logic refactors (about to test)

--- src/HelperFunctions.sol
+++ src/HelperFunctions.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-library Die {
+library HelperFunctions {
     function rollDie(bytes memory salt) external pure returns (uint8) {
         return (uint8(uint256(bytes32(abi.encodePacked(salt, bytes1(0xFF))))) % 6) + 1;
+    }
+
+    function computeTokenId(bytes memory salt) external pure returns (bytes32) {
+        return bytes32(keccak256(salt));
     }
 }
--- src/LudoGame.sol
+++ src/LudoGame.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import { Die } from "src/Die.sol";
+import { HelperFunctions } from "src/HelperFunctions.sol";
 
 contract LudoGame {
   // Errors
@@ -10,25 +10,52 @@ contract LudoGame {
   error LudoGame__RefundFailed();
   error LudoGame__GameAlreadyFull();
   error LudoGame__PlayingOutOfTurn();
+  error LudoGame__YouHaveNoMoreTokenLeft();
 
   // Events
   event PlayerJoined(address indexed player);
 
-  struct Player {
-    address addr;
+  struct Token {
+    bytes32 id;
+    address ownedBy;
     uint32 position;
   }
 
+  struct Player {
+    address addr;
+    uint8 tokenLeft;
+    Token tokenInPlay;
+  }
+
+
   uint public constant ENTRY_FEE = 1 ether;
+  uint public constant MAX_BOARD_LENGTH = (6 * (MAX_PLAYERS * 2)) + MAX_PLAYERS;
   uint8 public constant MAX_PLAYERS = 4;
+  uint8 public constant STARTING_TOKEN_COUNT = 4;
+
   uint8 private playersCount;
   bool private gameStarted = false;
   address private playerInTurn;
-  mapping(address => Player info) private playerInfo;
+
+  address[MAX_PLAYERS] private players;
+  mapping (address player => Player info) private playerInfo;
+  mapping (uint space => Token token) private tokenOnSpace;
   
   // Modifiers
   modifier onlyPlayerInTurn() {
     if (msg.sender != playerInTurn) revert LudoGame__PlayingOutOfTurn();
+
+    _;
+  }
+
+  function bringOutToken(Player storage player) internal {
+    if (player.tokenLeft == 0) revert LudoGame__YouHaveNoMoreTokenLeft();
+
+    player.tokenInPlay = Token({
+      id: HelperFunctions.computeTokenId(abi.encodePacked(player.addr, player.tokenLeft, block.timestamp)),
+      ownedBy: player.addr,
+      position: 0
+    });
   }
 
   function joinGame() external payable {
@@ -40,6 +67,7 @@ contract LudoGame {
     if (msg.value < ENTRY_FEE) revert LudoGame__NotEnoughEntryFee();
 
     player.addr = msg.sender;
+    players[playersCount] = msg.sender;
     playersCount = playersCount + 1;
 
     // Handle Refund of excess entry fee
@@ -56,14 +84,45 @@ contract LudoGame {
     emit PlayerJoined(msg.sender);
   }
 
+  function passTurn() internal {}
+
   function play() external onlyPlayerInTurn {
     Player storage player = playerInfo[msg.sender];
+    uint8 roll = HelperFunctions.rollDie(abi.encode(player.addr, block.timestamp, player.tokenInPlay));
 
-    uint8 roll = Die.rollDie(abi.encodePacked(player.addr, block.timestamp));
+    if (player.tokenInPlay.ownedBy == address(0)) {
+      if (player.tokenLeft > 0) {
+        if (roll == 6) bringOutToken(player);
+        else passTurn();
+      }
+      else revert LudoGame__YouHaveNoMoreTokenLeft();
+    }
 
+    uint32 spaceToLand = uint32((player.tokenInPlay.position + roll) % MAX_BOARD_LENGTH);
+
+    moveAndCapture(player, spaceToLand);
   }
 
-  function movePlayerPiece() private {}
+  function moveAndCapture(Player storage player, uint spaceToLand) private {
+    Token storage tokenOccupyingSpace = tokenOnSpace[spaceToLand];
+
+    // No Token on that space, procees as normal
+    if (tokenOccupyingSpace.ownedBy == address(0)) tokenOnSpace[spaceToLand] = player.tokenInPlay;
+    else {
+      // Capture Logic
+      Player storage capturedPlayer = playerInfo[tokenOccupyingSpace.ownedBy];
+      
+      capturedPlayer.tokenLeft = capturedPlayer.tokenLeft - 1;
+      
+      capturedPlayer.tokenInPlay = Token({
+        id: bytes32(0),
+        ownedBy: address(0),
+        position: 0
+      });
+
+      tokenOnSpace[spaceToLand] = player.tokenInPlay;
+    }
+  }
 
   // Getter Functions
   function getPlayerInfo(address player) external view returns (Player memory) {
@@ -83,6 +142,10 @@ contract LudoGame {
   }
 
   function getPlayerBoardPosition(address player) external view returns(uint32) {
-    return playerInfo[player].position;
+    return playerInfo[player].tokenInPlay.position;
+  }
+
+  function getTokenOnSpace(uint32 space) external view returns (Token memory) {
+    return tokenOnSpace[space];
   }
 }
